110bd0f3f653fee594370f6bff305e65ec597ab1
skyLaptop <antargtig@inbox.ru>
2025-09-19 11:37:16 +0300

    Function realised and working stabely. Added errors handling and right output.

--- main.py
+++ main.py
@@ -3,4 +3,19 @@ import requests
 from config import token
 headers = {"Authorization": f"token {token}"}
 
-print(requests.get("https://api.github.com/users/tafia/events"))
+def fetchActivity():
+    print("Enter name or profile URL:")
+    n = input().split("/")
+    response = (requests.get(f"https://api.github.com/users/{n[-1]}"))
+    if response.status_code != 200:
+        print("Wrong name or URL.")
+        return 0
+    data = (requests.get(f"https://api.github.com/users/{n[-1]}/events")).json()
+    if len(data) == 0:
+        print("No events found.")
+        return 0
+    print(f"Overall found {len(data)} events.\n")
+    for key in data:
+        print(f"Operation {key["type"][:-5]} was made with repo {key["repo"]["name"]} at {key["created_at"][0:10]} {key["created_at"][11:-1]}\n")
+
+fetchActivity()
